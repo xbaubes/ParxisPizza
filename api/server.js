1f412ff0b1a897ddffbec3a34a0f9c6e328e2c29
XBP <xavierbaubes@gmail.com>
2025-03-14 00:35:37 +0100

    use environment variable instead of hardcoded value

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ import app from './app.js';
 
 // Posar en marxa el servidor
 const port = process.env.PORT;
+const server = process.env.SERVER;
 app.listen(port, () => {
-    console.log(`🚀 Servidor en marxa a http://localhost:${port}`);
+    console.log(`🚀 Servidor en marxa a http://${server}:${port}`);
 });
