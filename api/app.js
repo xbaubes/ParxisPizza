f8b510b8fe855a49572a653c5589f3d6fc4b13f3
XBP <xavierbaubes@gmail.com>
2025-03-14 00:14:08 +0100

    App splitted from server

---
+++ api/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import { connectDB } from '../db/connect.js';
+import routes from './routes/routes.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.use('/api/v1', routes);
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+export default app;
--- api/server.js
+++ api/server.js
@@ -1,21 +1,5 @@
 import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from '../db/connect.js';
-import routes from './routes/routes.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { errorHandler } from './middlewares/errorHandler.js';
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-connectDB();
-
-app.use('/api/v1', routes);
-app.use(notFoundHandler);
-app.use(errorHandler);
+import app from './app.js';
 
 // Posar en marxa el servidor
 const port = process.env.PORT;
