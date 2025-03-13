51bcf5a5d9d46d33d90c92b58ccd04edb59837c9
XBP <xavierbaubes@gmail.com>
2025-03-14 00:51:13 +0100

    sql file to create admin user of db

---
+++ db/user.sql
@@ -0,0 +1,21 @@
+USE master;
+GO
+
+-- Crear un Login (usuari del servidor)
+CREATE LOGIN adminParxisPizza 
+WITH PASSWORD = 'CL1zmQy!asp.-M!',
+     DEFAULT_DATABASE = Pizzeria,
+     CHECK_EXPIRATION = OFF,
+     CHECK_POLICY = OFF;
+GO
+
+-- Assignar permisos a la base de dades específica
+USE Pizzeria;
+GO
+
+CREATE USER adminParxisPizza FOR LOGIN adminParxisPizza;
+GO
+
+-- Concedir permisos necessaris
+ALTER ROLE db_owner ADD MEMBER adminParxisPizza;
+GO
