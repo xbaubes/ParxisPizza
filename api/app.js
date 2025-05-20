302ba85ad105f111a445fd7065a068ff611cc267
Xavier Baubés Parramon <xavierbaubes@gmail.com>
2025-05-20 11:30:57 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -8,7 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors()); // Configuració per defecte, permet totes les peticions. És segur ???
 
 await connectDB();
 
